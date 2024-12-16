82066462e2b5441d24228bbe865c98ef8010d497
Ilja Lobossok <iljalobossok@gmail.com>
2024-12-16 10:26:21 +0200

    2 task

--- final/finalwork.py
+++ final/finalwork.py
@@ -8,3 +8,10 @@ def task1_check_age(age:int) -> str:
         return "welcome"
 
 
+"task 2"
+def task2_normalize_name(name: str) -> str:
+    cap_name = ""
+    for sub_name in name.split(" "):
+        cap_name += sub_name.capitalize() + " "
+    cap_name = cap_name[:-1]
+    return cap_name
